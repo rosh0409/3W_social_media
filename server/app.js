5b2be1fc93cac1ac95d8cdd0e6194a9f8179ba5d
rosh0409 <rosh0409singh@gmail.com>
2025-01-21 21:31:33 +0530

    done

--- server/app.js
+++ server/app.js
@@ -8,7 +8,12 @@ dotenv.config();
 
 const app = express();
 
-app.use(cors());
+app.use(
+  cors({
+    origin: "*",
+    methods: ["GET", "POST", "PUT", "DELETE"],
+  })
+);
 app.use(express.json());
 
 app.use("/user", userRoute);
